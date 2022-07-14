45750d9960672806914791e63b442b71d04577f9
Zeus <84690417+darkrut22@users.noreply.github.com>
2022-07-14 20:49:37 +0900

    integrated three.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+import { Canvas } from "@react-three/fiber";
 
 function App() {
+  
 
   return (
     <div className="w-full h-screen bg-fuchsia-100">
-      React Test
+      <Canvas shadows >
+        <color attach="background" args={['#444488']}/>
+        <fog attach="fog" color="#444488" near={50} far={300} />
+      </Canvas>
     </div>
   );
 }
