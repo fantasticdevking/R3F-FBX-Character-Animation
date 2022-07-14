4aab86e40efb851402fe7afe61f5e7b1c6870db4
Zeus <84690417+darkrut22@users.noreply.github.com>
2022-07-14 21:35:37 +0900

    added Character

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React, { Suspense } from "react";
 
 import * as THREE from "three";
 
+import { Loader, OrbitControls, softShadows } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { Loader, OrbitControls } from "@react-three/drei";
 
 import Ground from "./components/Ground";
+import Character from "./components/Character";
 import Nature from "./components/Nature";
 
+softShadows();
 function App() {
   const hemiLight = new THREE.HemisphereLight(0xffffff, 0xfffffff, 0.6);
   hemiLight.color.setHSL(0.6, 1, 0.6);
@@ -29,37 +31,34 @@ function App() {
   light.shadow.mapSize.height = 4096;
   light.shadow.camera.near = 0.1;
   light.shadow.camera.far = 500.0;
-  light.shadow.camera.near = 0.5;
-  light.shadow.camera.far = 500.0;
-  light.shadow.camera.left = 50;
-  light.shadow.camera.right = -50;
-  light.shadow.camera.top = 50;
-  light.shadow.camera.bottom = -50;
+  light.shadow.camera.left = 100;
+  light.shadow.camera.right = -100;
+  light.shadow.camera.top = 100;
+  light.shadow.camera.bottom = -100;
 
   return (
     <div className="w-full h-screen bg-fuchsia-100">
+      <Canvas shadows camera={camera}>
+        <color attach="background" args={['#444444']}/>
 
-        <Canvas shadows >
-          <color attach="background" args={['#555555']}/>
-          <fog attach="fog" color="#555555" near={50} far={300} />
+        <hemisphereLight {...hemiLight} />/
+        <directionalLight {...light} />
+        <ambientLight intensity={0.1} />
 
-          <hemisphereLight {...hemiLight} />/
-          <directionalLight {...light} />
-          <ambientLight intensity={0.1} />
+        <Suspense fallback={null}>
+          <Ground />
+          <perspectiveCamera {...camera} />
+          <Character camera={camera} />
+          <Nature />
+        </Suspense>
 
-          <OrbitControls />
+        <fog attach="fog" color="#444444" near={50} far={300} />
 
-          <Suspense fallback={null}>
-            <Ground />
-            <Nature />
-            <perspectiveCamera {...camera} />
-          </Suspense>
-        </Canvas>
-
-        <Loader
-          dataInterpolation={(p) => `Loading ${p.toFixed(2)}%`}
-          initialState={(active) => active}
-        />
+      </Canvas>
+      <Loader
+        dataInterpolation={(p) => `Loading ${p.toFixed(2)}%`}
+        initialState={(active) => active}
+      />
     </div>
   );
 }
---
+++ src/components/Character.tsx
@@ -0,0 +1,310 @@
+import { useFrame, useLoader } from "@react-three/fiber";
+import React, { useCallback, useEffect, useRef } from "react";
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
+import { useFBX } from "@react-three/drei";
+import * as THREE from "three";
+
+import { Mesh } from "three";
+
+interface Animations {
+  [name: string]: {
+    clip: THREE.AnimationAction;
+  };
+}
+
+interface CharacterProps {
+  camera: THREE.PerspectiveCamera;
+}
+const Character: React.FC<CharacterProps> = ({ camera }) => {
+  const character = useRef<Mesh>(null!);
+
+  const activeAnimation: {
+    forward: boolean;
+    backward: boolean;
+    left: boolean;
+    right: boolean;
+    run: boolean;
+    dance: boolean;
+  } = {
+    forward: false,
+    backward: false,
+    left: false,
+    right: false,
+    run: false,
+    dance: false,
+  };
+
+  const animations: Animations = {};
+
+  const currentPosition = new THREE.Vector3();
+  const currentLookAt = new THREE.Vector3();
+  const decceleration = new THREE.Vector3(-0.0005, -0.0001, -5.0);
+  const acceleration = new THREE.Vector3(1, 0.125, 100.0);
+  const velocity = new THREE.Vector3(0, 0, 0);
+
+  const c = useLoader(FBXLoader, "./assets/character/character.fbx");
+
+  c.scale.setScalar(0.1);
+  c.traverse((f: { castShadow: boolean; receiveShadow: boolean; }) => {
+    f.castShadow = true;
+    f.receiveShadow = true;
+  });
+
+  const mixer = new THREE.AnimationMixer(c);
+
+  const idle = useFBX("./assets/character/idle.fbx");
+
+  animations["idle"] = {
+    clip: mixer.clipAction(idle.animations[0]),
+  };
+
+  const walk = useFBX("./assets/character/walking.fbx");
+
+  animations["walk"] = {
+    clip: mixer.clipAction(walk.animations[0]),
+  };
+
+  const run = useFBX("./assets/character/running.fbx");
+
+  animations["run"] = {
+    clip: mixer.clipAction(run.animations[0]),
+  };
+
+  const dance = useFBX("./assets/character/dance.fbx");
+
+  animations["dance"] = {
+    clip: mixer.clipAction(dance.animations[0]),
+  };
+
+  // set current Action
+  let currAction = animations["idle"].clip;
+
+  let prevAction: THREE.AnimationAction;
+
+  // Controll Input
+  const handleKeyPress = useCallback((event) => {
+    switch (event.keyCode) {
+      case 87: //w
+        activeAnimation.forward = true;
+
+        break;
+
+      case 65: //a
+        activeAnimation.left = true;
+
+        break;
+
+      case 83: //s
+        activeAnimation.backward = true;
+
+        break;
+
+      case 68: // d
+        activeAnimation.right = true;
+
+        break;
+
+      case 69: //e dance
+        activeAnimation.dance = true;
+
+        break;
+      case 16: // shift
+        activeAnimation.run = true;
+        break;
+    }
+  }, []);
+
+  const handleKeyUp = useCallback((event) => {
+    switch (event.keyCode) {
+      case 87: //w
+        activeAnimation.forward = false;
+        break;
+
+      case 65: //a
+        activeAnimation.left = false;
+        break;
+
+      case 83: //s
+        activeAnimation.backward = false;
+        break;
+
+      case 68: // d
+        activeAnimation.right = false;
+        break;
+
+      case 69: //e dance
+        activeAnimation.dance = false;
+        break;
+
+      case 16: // shift
+        activeAnimation.run = false;
+        break;
+    }
+  }, []);
+
+  const calculateIdealOffset = () => {
+    const idealOffset = new THREE.Vector3(0, 20, -30);
+    idealOffset.applyQuaternion(character.current.quaternion);
+    idealOffset.add(character.current.position);
+    return idealOffset;
+  };
+
+  const calculateIdealLookat = () => {
+    const idealLookat = new THREE.Vector3(0, 10, 50);
+    idealLookat.applyQuaternion(character.current.quaternion);
+    idealLookat.add(character.current.position);
+    return idealLookat;
+  };
+
+  function updateCameraTarget(delta: number) {
+    const idealOffset = calculateIdealOffset();
+    const idealLookat = calculateIdealLookat();
+
+    const t = 1.0 - Math.pow(0.001, delta);
+
+    currentPosition.lerp(idealOffset, t);
+    currentLookAt.lerp(idealLookat, t);
+
+    camera.position.copy(currentPosition);
+  }
+
+  // movement
+  const characterState = (delta: number) => {
+    const newVelocity = velocity;
+    const frameDecceleration = new THREE.Vector3(
+      newVelocity.x * decceleration.x,
+      newVelocity.y * decceleration.y,
+      newVelocity.z * decceleration.z
+    );
+    frameDecceleration.multiplyScalar(delta);
+    frameDecceleration.z =
+      Math.sign(frameDecceleration.z) *
+      Math.min(Math.abs(frameDecceleration.z), Math.abs(newVelocity.z));
+
+    newVelocity.add(frameDecceleration);
+
+    const controlObject = character.current;
+    const _Q = new THREE.Quaternion();
+    const _A = new THREE.Vector3();
+    const _R = controlObject.quaternion.clone();
+
+    const acc = acceleration.clone();
+    if (activeAnimation.run) {
+      acc.multiplyScalar(2.0);
+    }
+
+    if (currAction === animations["dance"].clip) {
+      acc.multiplyScalar(0.0);
+    }
+
+    if (activeAnimation.forward) {
+      newVelocity.z += acc.z * delta;
+    }
+    if (activeAnimation.backward) {
+      newVelocity.z -= acc.z * delta;
+    }
+    if (activeAnimation.left) {
+      _A.set(0, 1, 0);
+      _Q.setFromAxisAngle(_A, 4.0 * Math.PI * delta * acceleration.y);
+      _R.multiply(_Q);
+    }
+    if (activeAnimation.right) {
+      _A.set(0, 1, 0);
+      _Q.setFromAxisAngle(_A, 4.0 * -Math.PI * delta * acceleration.y);
+      _R.multiply(_Q);
+    }
+
+    controlObject.quaternion.copy(_R);
+
+    const oldPosition = new THREE.Vector3();
+    oldPosition.copy(controlObject.position);
+
+    const forward = new THREE.Vector3(0, 0, 1);
+    forward.applyQuaternion(controlObject.quaternion);
+    forward.normalize();
+
+    const sideways = new THREE.Vector3(1, 0, 0);
+    sideways.applyQuaternion(controlObject.quaternion);
+    sideways.normalize();
+
+    sideways.multiplyScalar(newVelocity.x * delta);
+    forward.multiplyScalar(newVelocity.z * delta);
+
+    controlObject.position.add(forward);
+    controlObject.position.add(sideways);
+
+    character.current.position.copy(controlObject.position);
+    updateCameraTarget(delta);
+  };
+
+  useFrame((state, delta) => {
+    prevAction = currAction;
+
+    if (activeAnimation.forward) {
+      if (activeAnimation.run) {
+        currAction = animations["run"].clip;
+      } else {
+        currAction = animations["walk"].clip;
+      }
+    } else if (activeAnimation.left) {
+      if (activeAnimation.run) {
+        currAction = animations["run"].clip;
+      } else {
+        currAction = animations["walk"].clip;
+      }
+    } else if (activeAnimation.right) {
+      if (activeAnimation.run) {
+        currAction = animations["run"].clip;
+      } else {
+        currAction = animations["walk"].clip;
+      }
+    } else if (activeAnimation.backward) {
+      if (activeAnimation.run) {
+        currAction = animations["run"].clip;
+      } else {
+        currAction = animations["walk"].clip;
+      }
+    } else if (activeAnimation.dance) {
+      currAction = animations["dance"].clip;
+    } else {
+      currAction = animations["idle"].clip;
+    }
+
+    if (prevAction !== currAction) {
+      prevAction.fadeOut(0.2);
+
+      if (prevAction === animations["walk"].clip) {
+        const ratio =
+          currAction.getClip().duration / prevAction.getClip().duration;
+        currAction.time = prevAction.time * ratio;
+      }
+
+      currAction.reset().play();
+    } else {
+      currAction.play();
+    }
+
+    characterState(delta);
+    const idealLookat = calculateIdealLookat();
+
+    state.camera.lookAt(idealLookat);
+    state.camera.updateProjectionMatrix();
+    mixer?.update(delta);
+  });
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyPress);
+
+    document.addEventListener("keyup", handleKeyUp);
+    currAction.play();
+    return () => {
+      document.removeEventListener("keydown", handleKeyPress);
+
+      document.removeEventListener("keyup", handleKeyUp);
+    };
+  });
+
+  return <primitive object={c} ref={character} />;
+};
+
+export default Character;
